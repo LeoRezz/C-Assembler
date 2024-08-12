712dae59371f26626989a130875a7e04d42a0848
LeoRezz <liormor97@gmail.com>
2024-08-12 23:18:37 +0300

    Fixed bug: lea opcode shown as "NOT" type

--- New_tokenizer_moudle/tokenizer.c
+++ New_tokenizer_moudle/tokenizer.c
@@ -33,7 +33,7 @@ static const char *reserved_words[] = {
     "r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7",
 
     /* Opcodes (Instructions) */
-    "mov", "cmp", "add", "sub", "not", "clr", "lea",
+    "mov", "cmp", "add", "sub", "lea", "clr", "not",
     "inc", "dec", "jmp", "bne", "red", "prn", "jsr", "rts", "stop",
 
     /* Directives */
