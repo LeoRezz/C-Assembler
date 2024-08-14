f72f5ce1a20504b5a803d0338bfe5c0ec31d7dc2
LeoRezz <Liormor97@gmail.com>
2024-08-14 10:26:37 +0300

    Revert "Add files via upload"
    This reverts commit e2e2b08f53462884bb9fb46e7c8cc202d9ebcba6.

--- viki_src/parser.h
+++
@@ -1,8 +0,0 @@
-#include "symbolTable.h"
-#include "opcodeTable.h"
-#include "util.h"
-#include "tokenizer.h"
-
-Line *parse_line(Token *token_arr, int token_count);
-void print_parsed_line(Line *parsed_line);
-int calculate_instruction_words(int opcode);
